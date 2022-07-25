0da60e4e66492999fad6f23e95c7ad61f5b61802
CMonjo <camille@cmonjo.com>
2022-07-25 13:57:26 +0200

    feat(plugin): added upload feature

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,22 @@
-module.exports = ({}) => ({
+module.exports = ({ }) => ({
     'wysiwyg': {
         enabled: true,
         resolve: './src/plugins/wysiwyg'
     },
     'users-permissions': {
         config: {
-          jwt: {
-            expiresIn: '30d',
-          },
+            jwt: {
+                expiresIn: '30d',
+            },
         },
-      },
+    },
+    'upload': {
+        config: {
+            providerOptions: {
+                localServer: {
+                    maxage: 300000
+                },
+            },
+        },
+    },
 });
